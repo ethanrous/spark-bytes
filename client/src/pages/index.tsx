8574a99cbb49de16cb51343908f252df54e5e4bb
Jude Lopez <judecl@bu.edu>
2024-11-18 00:57:28 -0500

    minor changes

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -14,7 +14,7 @@ const Home: React.FC = () => {
     position: 'relative',
     height: '100vh', 
     width: '100%', 
-    backgroundImage: `url('/assets/green_planet.jpeg')`,
+    backgroundImage: `url('/assets/food-table.jpg')`,
     backgroundSize: 'cover', 
     backgroundPosition: 'center', 
     color: '#FFFBE6',
@@ -24,8 +24,8 @@ const Home: React.FC = () => {
 
   const heroTextStyle: React.CSSProperties = {
     position: 'absolute',
-    top: '30%',
-    left: '10%',
+    top: '10%',
+    left: '7%',
     maxWidth: '400px',
     color: '#FFFBE6',
     fontFamily: '"Josefin Sans", sans-serif',
@@ -49,7 +49,7 @@ const Home: React.FC = () => {
     textAlign: 'center',
     fontFamily: '"Josefin Sans", sans-serif',
     color: '#264039',
-    marginBottom,
+    // marginBottom,
   });
 
   return (
@@ -57,13 +57,32 @@ const Home: React.FC = () => {
       <Header />
       <main style={mainStyle}>
         <section style={heroSectionStyle}>
+        {/* 
+        TODO: 
+        -fix so placement of everything is consistent with all window sizes  
+        -make button hoverable
+        */}
           <div style={heroTextStyle}>
-            <h1 style={{ fontSize: '36px', marginBottom: '20px' }}>Sustainability at its core</h1>
-            <p style={{ fontSize: '18px', marginBottom: '20px' }}>Turn Leftovers into Opportunities: Free Food, Zero Waste</p>
-            <button style={buttonStyle}>Find Events</button>
+            <h1 style={{ fontSize: '36px', marginBottom: '20px' }}>Sustainability at Its Core</h1>
+            <p style={{ fontSize: '18px', marginBottom: '20px' }}>Turn leftovers into opportunities. <br/> Free food, zero waste.</p>
+            <a href="/view-events">
+              <button style={buttonStyle} id="find-events">Find Events</button>
+            </a>
           </div>
+          {/* 
+          TODO: 
+          -place in all photos, ensure correct scaling
+          -placement in bottom right or smth similar
+          -same thing with location consistency accross window sizes
+          -consider making this a component
+          */}
+          {/* SCALING AND LOCATION ARE TEMPORARY */}
+          <a href="https://www.instagram.com/bostonuniversitygms/?hl=en" target="_blank" style={{textDecoration: "none", display: "flex", alignItems: "center", position: "relative", top: "95%", left: "86%"}}>
+            <img src="../assets/instagram.svg" style={{width: "2.5%", height: "auto", paddingRight: 8}} />
+            <p style={{color: "white"}}>@bostonuniversitygms</p>
+          </a>
         </section>
-
+        
         {/* Additional sections */}
         <section style={sectionStyle('#D5ED9F')}>
           <h2>Discover Events Near You</h2>
@@ -74,7 +93,7 @@ const Home: React.FC = () => {
           <p>Create events and share your leftovers with the BU community.</p>
         </section>
         <section style={sectionStyle('#FF9100', '40px')}> 
-          <h2>Why Spark! Bytes?</h2>
+          <h2>Why 'Spark! Bytes'?</h2>
           <p>Making sustainability simple, accessible, and delicious.</p>
         </section>
       </main>
