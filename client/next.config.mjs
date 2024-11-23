3e9d93231e0ae5dc91b2f668a9825b645d5c94e4
EthRousseau <ethanrousseau99+github@gmail.com>
2024-11-22 21:57:29 -0500

    WIP

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  source: '/api/:path*',
-  destination: 'http://localhost:5000/api/:path*'
+  async redirects() {
+    return [{
+      source: '/api/:path*',
+      destination: 'http://localhost:5000/api/:path*',
+      permanent: false
+    }]
+  },
 }
 
 export default nextConfig
